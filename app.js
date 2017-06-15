e7969bf95dfc6bc2afec870dd30348ceb251968c
Woo Yun <woocyun@gmail.com>
2017-06-14 20:03:33 -0700

    basic route to request lotteries data

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-const jackpotUpdater = require('./jackpotUpdater');
+const express = require('express');
+const fs = require('fs');
 
-jackpotUpdater();
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send('hello world!');
+});
+
+app.get('/lotteries', (req, res) => {
+  fs.readFile('data/lotteries.json', 'utf-8', (err, data) => {
+    if (!err) {
+      res.json(JSON.parse(data));
+    } else {
+      res.status(500).send('aw crap');
+    }
+  });
+});
+
+app.listen(port);
+
+require('./jackpotScraper')();
